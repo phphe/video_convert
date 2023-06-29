336791a17a2781f83194e791992985177bdbaf3a
phphe <phphe@outlook.com>
2023-06-30 00:23:44 +0800

    add logs dir

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 pm2 stop video_convert  # Stop the running app
-pm2 start index.js --name "video_convert" -i max
+pm2 start index.js --name "video_convert" -i max --output ./logs/out.log --error ./logs/error.log --time --merge-logs
